f4ef406301aadb68787aab829fe852b0d7e30d60
David <sykesdavid8@gmail.com>
2020-09-21 18:42:59 +0000

    inital tables setup and intial user data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,22 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+
+INSERT INTO users (name, email, password) VALUES ('Alice', 'lostin@wonderland.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Kira', 'bendit@beckham.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Jordan', 'caleb@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Ellen', 'ripley@lv426.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Kyle', 'reese@future.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Hasan', 'minhaj@patriot.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Jose', 'martinez@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Ava', 'duvarney@yahoo.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Selma', 'letreign@freedom.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Roger', 'fred@hotmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Salvadore', 'madness@dali.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Trog', 'dorThe@burninator.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Alyesha', 'bendit@beckham.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Jen', 'wentworth@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Chris', 'evans@americasass.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Kamala', 'khan@secrets.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Miles', 'morales@secrets.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Amadeus', 'chou@secrets.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Shang', 'chi@yahoo.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Luc', 'besson@gmail.com', 'password');
