21b117de5f896746fd8afa53cbe2e8ab51c3f38a
Maria <mariakhandaker@gmail.com>
2020-09-22 01:33:41 +0000

    adding some temp view files

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,9 +1,33 @@
 const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("choice-text"));
+const choices = Array.from(document.getElementsByClassName("choice"));
 console.log(choices);
 
 let currentQuestion = {};
 let acceptingAnswers = true;
-let score = 0;
-let questionCounter = 0;
+let questionCount = 0;
 let availableQuestions = [];
+let questions = [];
+
+//CONSTANTS
+startGame = () => {
+  questionCount = 0;
+  console.log(availableQuestions)
+  getNewQuestion();
+};
+
+getNewQuestion = () => {
+  questionCount++;
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion - availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+    choices.forEach( choice => {
+      choice.innerText = currentQuestion['choice' + number];
+    })
+}
+
+scoreUp = num => {
+  score += num;
+  scoreText.innerText = score;
+};
+
+startGame();
