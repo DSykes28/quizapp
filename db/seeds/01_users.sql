539231190b275e20994bc78412e258a696aaadaa
David <sykesdavid8@gmail.com>
2020-09-23 04:00:25 +0000

    adding fixed code to confirm data seeds the db

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -22,17 +22,17 @@ INSERT INTO users (name, email, password) VALUES ('Shang', 'chi@yahoo.com', 'pas
 INSERT INTO users (name, email, password) VALUES ('Luc', 'besson@gmail.com', 'password');
 
 
-INSERT INTO quiz_names (user_id, title, private, total_score) VALUES ('4', 'Aliens Movie Trivia');
-INSERT INTO quiz_names (user_id, title, private, total_score) VALUES ('9', 'Country capitals!');
-INSERT INTO quiz_names (user_id, title, private, total_score) VALUES ('17', '2020 Music');
-INSERT INTO quiz_names (user_id, title, private, total_score) VALUES ('20', 'Foreign Film');
-INSERT INTO quiz_names (user_id, title, private, total_score) VALUES ('7', 'Expanded Star Wars Trivia');
-INSERT INTO quiz_names (user_id, title, private, total_score) VALUES ('7', 'Star Trek Trivia');
-INSERT INTO quiz_names (user_id, title, private, total_score) VALUES ('1', 'Sci-Fi Authors');
-INSERT INTO quiz_names (user_id, title, private, total_score) VALUES ('1', 'Fantasy Authors', TRUE);
-INSERT INTO quiz_names (user_id, title, private, total_score) VALUES ('13', 'State capitals');
-INSERT INTO quiz_names (user_id, title, private, total_score) VALUES ('3', 'Golden Girl Quotes');
-INSERT INTO quiz_names (user_id, title, private, total_score) VALUES ('16', 'How well do you know - The Big Lebowski?');
+INSERT INTO quiz_names (user_id, title, private) VALUES ('4', 'Aliens Movie Trivia', FALSE);
+INSERT INTO quiz_names (user_id, title, private) VALUES ('9', 'Country capitals!', FALSE);
+INSERT INTO quiz_names (user_id, title, private) VALUES ('17', '2020 Music', FALSE);
+INSERT INTO quiz_names (user_id, title, private) VALUES ('20', 'Foreign Film', FALSE);
+INSERT INTO quiz_names (user_id, title, private) VALUES ('7', 'Expanded Star Wars Trivia', FALSE);
+INSERT INTO quiz_names (user_id, title, private) VALUES ('7', 'Star Trek Trivia', FALSE);
+INSERT INTO quiz_names (user_id, title, private) VALUES ('1', 'Sci-Fi Authors', FALSE);
+INSERT INTO quiz_names (user_id, title, private) VALUES ('1', 'Fantasy Authors', TRUE);
+INSERT INTO quiz_names (user_id, title, private) VALUES ('13', 'State capitals', FALSE);
+INSERT INTO quiz_names (user_id, title, private) VALUES ('3', 'Golden Girl Quotes', FALSE);
+INSERT INTO quiz_names (user_id, title, private) VALUES ('16', 'How well do you know - The Big Lebowski?', FALSE);
 
 
 
@@ -57,13 +57,13 @@ INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) V
 INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (3, 'Who sings State of the Union?', 'Nas', 'Joyner Lucas', 'Lil Baby', 'Public Enemy');
 INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (3, 'Which song does not feature Drake?', 'Only You Freestyle', 'Popstar', 'Greece', 'Rockstar');
 INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (3, 'Which sone appeared on Bill and Ted Face the Music?', 'Hero', 'California Snow', 'The End of the Game', 'Beginning of the End');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (4, 'Which Film won best Foreign Film at the 1999 Oscars?', 'Crouching Tiger, Hidden Dragon', "No Man's Land", 'Life is Beautiful', 'All About My Mother');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (4, 'Which Film won best Foreign Film at the 1999 Oscars?', 'Crouching Tiger, Hidden Dragon', 'No Man''s Land', 'Life is Beautiful', 'All About My Mother');
 INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (4, 'Who directed the French film Crimson River?', 'Luc Besson', 'Jean-Luc Godard', 'Jean-Jacques Annaud', 'Mathieu Kassovitz');
 INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (4, 'Which director helmed the German film Das Boot?', 'Oliver Hirschbiegel', 'Florian Henckel von Donnersmarck', 'Werner Herzog', 'Wolfgang Petersen');
 INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (4, 'Who directed Train to Busan?', 'Jee-woon Kim', 'Bong Joon Ho', 'Hoon-jung Park', 'Sang-ho Yeon');
 INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (4, 'Who was the director of Audition?', 'Shion Sono', 'Takeshi Kitano', 'Hideo Nakata', 'Takashi Miike');
 INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (5, 'Which character did Warwick Davis play in Return of the Jedi?', 'Admiral Piett', 'Nien Nunb', 'Admiral Ackbar', 'Wicket');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (5, "What was the name of Kyle Katarn's first ship?", 'The Outrider', 'IG-2000', 'Sun Crusher', 'Moldy Crow');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (5, 'What was the name of Kyle Katarn''s first ship?', 'The Outrider', 'IG-2000', 'Sun Crusher', 'Moldy Crow');
 INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (5, 'Which of the following is not a child of Leia and Han in the now discontinued expanded universe?', 'Jacen', 'Jaina', 'Anakin', 'Ben');
 INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (5, 'What was the cause of death of Chewbacca?', 'Finally hunted down by Bossk', 'Shot in the back by a Stormtrooper', 'Eaten by a sand worm', 'A moon fell on him');
 INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (5, 'Where did Luke train his students?', 'Coruscant', 'Hoth', 'Myrkr', 'Yavin 4');
@@ -88,23 +88,23 @@ INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) V
 INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (9, 'What is the capital of Delaware?', 'Georgetown', 'Millsboro', 'Wilmington', 'Dover');
 INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (9, 'What is the capital of South Dakota?', 'Bismarck', 'Sioux Falls', 'Rapid City', 'Pierre');
 INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (9, 'What is the capital of West Virginia?', 'Richmond', 'Huntington', 'Clarksburg', 'Charleston');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, 'Who said, "I could get herpes listening to this story!"', 'Blanche', 'Rose', 'Sophia', 'Dorothy');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, 'Who said, "When I was a child, I used to get overexcited and pet the cat too hard."', 'Dorothy', 'Sophia', 'Blanche', 'Rose');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, 'Who said, "Beat it, you 50-year old mattress!"', 'Blanche', 'Rose', 'Dorothy', 'Sophia');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, "Who said, 'You're a furry little gnome and we feed you too much!'", 'Blanche', 'Rose', 'Sophia', 'Dorothy');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, "Who said, 'My mistake; I thought since you looked like Yoda, you were also wise'?", 'Rose', 'Sophia', 'Dorothy', 'Blanche');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, "Who said, 'I feel like crawling under the covers and eating Velveeta right out of the box'?", 'Dorothy', 'Sophia', 'Blanche', 'Rose');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, "Who said, 'Get back here, you deceitful little Sicilian gecko!'", 'Rose', 'Sophia', 'Blanche', 'Dorothy');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, "Who said, 'Thank you, you bed hopping relic'?", 'Dorothy', 'Rose', 'Blanche', 'Sophia');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, "Who said, 'It's like life is a giant weenie roast, and I'm the biggest weenie!'", 'Dorothy', 'Sophia', 'Blanche', 'Rose');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, "Who said, 'When I say 'Jump' you say 'On who?'", 'Dorothy', 'Sophia', 'Rose', 'Blanche');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (11, "What Bob Dylan song plays during the film's opening credits?", 'Lay Lady Lay', 'Like a Rolling Stone', 'RoseForever Young', 'The Man in Me');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (11, "What is The Dude's favorite drink?", 'Lay Lady Lay', 'Like a Rolling Stone', 'Forever Young', 'White Russian (a.k.a. Caucasian)');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (11, "In what year does The Big Lebowski take place?", '1999', '1988', '1994', '1991');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (11, "What prolific Western actor narrates the film as The Stranger?", 'Jeff Bridges', 'Robert Duvall', 'Clint Eastwood', 'Sam Elliot');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (11, "For what single item does The Dude write a check totaling 69 cents at the supermarket in the opening sequence?", 'An Orange', 'Candy Bar', 'Beef Jerky', 'Quart of Milk');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (11, "What religion does Walter practice?", 'Catholocism', 'Southern Baptist', 'Islam', 'Judaism');
-INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (11, "In what war did Jeffrey Lebowski (a.k.a. The Big Lebowski) lose the use of his legs?", 'World War II', 'World War I', 'Vietnam War', 'Korean War');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, 'Who said, ''I could get herpes listening to this story!''', 'Blanche', 'Rose', 'Sophia', 'Dorothy');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, 'Who said, ''When I was a child, I used to get overexcited and pet the cat too hard.''', 'Dorothy', 'Sophia', 'Blanche', 'Rose');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, 'Who said, ''Beat it, you 50-year old mattress!''', 'Blanche', 'Rose', 'Dorothy', 'Sophia');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, 'Who said, ''You''re a furry little gnome and we feed you too much!''', 'Blanche', 'Rose', 'Sophia', 'Dorothy');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, 'Who said, ''My mistake; I thought since you looked like Yoda, you were also wise''?', 'Rose', 'Sophia', 'Dorothy', 'Blanche');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, 'Who said, ''I feel like crawling under the covers and eating Velveeta right out of the box''?', 'Dorothy', 'Sophia', 'Blanche', 'Rose');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, 'Who said, ''Get back here, you deceitful little Sicilian gecko!''', 'Rose', 'Sophia', 'Blanche', 'Dorothy');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, 'Who said, ''Thank you, you bed hopping relic''?', 'Dorothy', 'Rose', 'Blanche', 'Sophia');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, 'Who said, ''It''s like life is a giant weenie roast, and I''m the biggest weenie!''', 'Dorothy', 'Sophia', 'Blanche', 'Rose');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (10, 'Who said, ''When I say ''Jump'' you say ''On who?''', 'Dorothy', 'Sophia', 'Rose', 'Blanche');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (11, 'What Bob Dylan song plays during the film''s opening credits?', 'Lay Lady Lay', 'Like a Rolling Stone', 'RoseForever Young', 'The Man in Me');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (11, 'What is The Dude''s favorite drink?', 'Lay Lady Lay', 'Like a Rolling Stone', 'Forever Young', 'White Russian (a.k.a. Caucasian)');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (11, 'In what year does The Big Lebowski take place?', '1999', '1988', '1994', '1991');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (11, 'What prolific Western actor narrates the film as The Stranger?', 'Jeff Bridges', 'Robert Duvall', 'Clint Eastwood', 'Sam Elliot');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (11, 'For what single item does The Dude write a check totaling 69 cents at the supermarket in the opening sequence?', 'An Orange', 'Candy Bar', 'Beef Jerky', 'Quart of Milk');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (11, 'What religion does Walter practice?', 'Catholocism', 'Southern Baptist', 'Islam', 'Judaism');
+INSERT INTO quizzes (title_id, question, choice_a, choice_b, choice_c, answer) VALUES (11, 'In what war did Jeffrey Lebowski (a.k.a. The Big Lebowski) lose the use of his legs?', 'World War II', 'World War I', 'Vietnam War', 'Korean War');
 
 INSERT INTO results (user_id, quiz_id, score, total_score) VALUES (9, 4, 5, 5);
 INSERT INTO results (user_id, quiz_id, score, total_score) VALUES (1, 11, 5, 7);
