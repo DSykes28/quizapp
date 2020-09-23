79ce36783fae9f54eb68c1191d42a2456c2e2189
Maria <mariakhandaker@gmail.com>
2020-09-23 04:38:33 +0000

    updated my views to match Affaf's views, rerouted a few things

--- routes/users.js
+++ routes/users.js
@@ -35,7 +35,8 @@ app.get("/",(req, res) => {
 });
 
 //form for user to make quiz
-app.get("/quiz/new", (req, res) => {
+app.route("/quiz/new")
+.get((req, res) => {
   const user = req.session.user_id;
   if (!user) {
     res.redirect('/login_register');
@@ -45,40 +46,53 @@ app.get("/quiz/new", (req, res) => {
     };
     res.render("new_quiz", templateVars);
   }
-});
-//quiz url - can be unlisted or public
-app.get("/quiz/:quizID", (req, res) => {
-
-});
-//page for user to view all of the quizzes they created
-app.get("")
-//upon completion of quiz, shareable link to result of attempt
-//show all quizzes - public - you don’t need to login to view or take quiz, only to make 1
-app.get("")
-//login
-//register
-app.post("/quiz/new", (req, res) => {
-  const quiz = req.params.title;
+})
+.post((req, res) => {
+  const quiz = req.params.new_quiz;
   const user = req.session.user_id
-
   if (!user) {
     res.send("Please log in to view.").redirect("/login");
   }
+  res.render("quizzes_view");
+});
+
+//quiz url - can be unlisted or public
+app.get("/quiz/:quizID", (req, res) => {
+  res.render("results");
+});
+
+//page for user to view all of the quizzes they created
+app.route("/login")
+  .get((req, res) => {
+  res.render("login");
+}).post((req, res) => {
+  //verify email using some helper function, then set cookie, then
+  res.render("user_homepage");
 })
+//upon completion of quiz, shareable link to result of attempt
+//show all quizzes - public - you don’t need to login to view or take quiz, only to make 1
+app.get("/results", (req, res) => {
+  res.render("result");
+});
 
-app.post("/register", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  if (email === '' || password === '') {
 
-  } else if (getUserByEmail(email. users)) {
+app.route("/register")
+  .get((req, res) => {
+    res.render("register");
+  })
+  .post((req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    if (email === '' || password === '') {
 
-  } else {
-    //register user in database. how?
-  }
-  req.session.user_id = email;
-  res.redirect("/quizzes_view");
-})
+    } else if (getUserByEmail(email. users)) {
+
+    } else {
+      //register user in database. how?
+    }
+    req.session.user_id = email;
+    res.redirect("/quizzes_view");
+  });
 
 app.post("/logout", (req, res) => {
   req.session = null;
