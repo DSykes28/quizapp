19b0d5aa0ceaf4cee491aa5af000d1de51a7afd4
Maria Khandaker <mariakhandaker@Marias-MacBook-Pro.local>
2020-09-23 23:26:46 -0400

    modifying routes yet again

--- routes/users.js
+++ routes/users.js
@@ -27,6 +27,7 @@ module.exports = (db) => {
   router.get("/",(req, res) => {
     if (req.session) {
       res.redirect("/index");
+      //select 9 random quizzes to display through DB then show them in view
     } else {
       res.redirect("/login");
     }
@@ -45,6 +46,7 @@ module.exports = (db) => {
   });
 
   router.post("quiz/new",(req, res) => {
+    //insert new quiz into quiz database
     const quiz = req.params.new_quiz;
     if (req.session !== true) {
       res.send("Please log in to view.").redirect("/login");
@@ -54,12 +56,24 @@ module.exports = (db) => {
   });
 
   router.get("/quiz/:quizID", (req, res) => {
-    res.render("quiz");
+    //db query which quiz it is, then display it in a template var?
+    res.render("quiz", templateVars);
+  });
+
+  router.get("/user", (req, res) => {
+    //db query which user it is, then display quizzes created in a template var?
+    res.render("user", templateVars);
   });
 
   router.get("/user/:quizID", (req, res) => {
-    res.render("result");
+//db query - look in user to find which quiz result ^ route neets to change
+    res.render("result", templateVars);
   });
+
+  router.get("/result/:id", (req, res) => {
+    //db query to find result, name of quiz, score, stored in template vars
+    res.render("result", templateVars);
+  })
 
   return router;
 };
@@ -67,10 +81,8 @@ module.exports = (db) => {
 
 //form for user to make quiz
 
-
 //quiz url - can be unlisted or public
 ;
-
 //page for user to view all of the quizzes they created
 //upon completion of quiz, shareable link to result of attempt
 //show all quizzes - public - you don’t need to login to view or take quiz, only to make 1
