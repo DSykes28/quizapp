27ac11214b6f1029ba8e52b5d42ec9ef88329012
Maria Khandaker <mariakhandaker@Marias-MacBook-Pro.local>
2020-09-24 11:44:53 -0400

    "trying to merge some conflicts"

--- db/queries/select_9_quizzes.sql
+++ db/queries/select_9_quizzes.sql
@@ -2,14 +2,4 @@ SELECT title
 FROM quiz_names
 WHERE private = false
 ORDER BY random_id
-<<<<<<< HEAD:db/queries/select_9_quizzes.sql
-limit 9;
-
-=======
-limit 9;
-<<<<<<< HEAD
->>>>>>> master:db/queries/select_9_quizzes.sql
-=======
-
->>>>>>> 8322bb9b78f376e1577cd5e3090d2f643000b015:db/queries/select_9_quizzes.sql
->>>>>>> e9447df24396a5e01a98f1aa20839df30b42c11d
+LIMIT 9;
--- db/queries/select_quiz_questions_and_choices_by_quiz_name.sql
+++ db/queries/select_quiz_questions_and_choices_by_quiz_name.sql
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 SELECT quiz_names.id, question, choice_a AS A, choice_b AS B, choice_c as C, answer AS D, total_score
 FROM quiz_names
-=======
-SELECT quiz_names.id, question, choice_a AS A, choice_b AS B, choice_c as C, answer AS D, total_score FROM quiz_names
->>>>>>> master
-=======
-SELECT quiz_names.id, question, choice_a AS A, choice_b AS B, choice_c as C, answer AS D, total_score FROM quiz_names
-=======
-SELECT quiz_names.id, question, choice_a AS A, choice_b AS B, choice_c as C, answer AS D, total_score
-FROM quiz_names
->>>>>>> 8322bb9b78f376e1577cd5e3090d2f643000b015
->>>>>>> e9447df24396a5e01a98f1aa20839df30b42c11d
 JOIN quizzes ON title_id = user_id
 WHERE quiz_names.id = 1;
 
