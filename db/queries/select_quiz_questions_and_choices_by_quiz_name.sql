e63f619d14dca0116edcaed3bbe5e8ef50b8250c
David <sykesdavid8@gmail.com>
2020-09-24 22:55:25 +0000

    correcting pathing

--- db/queries/select_quiz_questions_and_choices_by_quiz_name.sql
+++ db/queries/select_quiz_questions_and_choices_by_quiz_name.sql
@@ -1,5 +1,4 @@
 SELECT quiz_names.id, question, choice_a AS A, choice_b AS B, choice_c as C, answer AS D, total_score
 FROM quiz_names
-JOIN quizzes ON title_id = user_id
-WHERE quiz_names.id = 1;
-
+JOIN quizzes ON quiz_names.id = title_id
+WHERE quiz_names.id = 10;
